0deabf5eb93c5cb466c110253974fba0f195cc46
Võ Trí Hiệu <hieu981.vn@gmail.com>
2026-03-05 02:03:28 +0700

    Create cource service and repository

---
+++ src/main/java/vn/edu/ute/service/CourseService.java
@@ -0,0 +1,13 @@
+package vn.edu.ute.service;
+
+import vn.edu.ute.model.Course;
+import java.util.List;
+
+public interface CourseService {
+    List<Course> getAllCourses();
+    Course addCourse(Course course);
+    boolean removeCourse(Long id);
+    // 2 hàm dùng Lambda:
+    List<Course> searchCourseByName(String keyword);
+    List<Course> getCoursesByLevel(String level);
+}
---
+++ src/main/java/vn/edu/ute/service/impl/CourseServiceImpl.java
@@ -0,0 +1,50 @@
+package vn.edu.ute.service.impl;
+
+import vn.edu.ute.model.Course;
+import vn.edu.ute.repository.CourseRepository;
+import vn.edu.ute.repository.impl.CourseRepositoryImpl;
+import vn.edu.ute.service.CourseService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CourseServiceImpl implements CourseService {
+    private final CourseRepository courseRepo = new CourseRepositoryImpl();
+
+    @Override
+    public List<Course> getAllCourses() {
+        return courseRepo.findAll();
+    }
+
+    @Override
+    public Course addCourse(Course course) {
+        return courseRepo.save(course);
+    }
+
+    @Override
+    public boolean removeCourse(Long id) {
+        return courseRepo.delete(id);
+    }
+
+
+
+    @Override
+    public List<Course> searchCourseByName(String keyword) {
+        List<Course> allCourses = courseRepo.findAll();
+        if (allCourses == null) return List.of();
+
+        return allCourses.stream()
+                .filter(c -> c.getCourseName().toLowerCase().contains(keyword.toLowerCase())) // tìm theo tn
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Course> getCoursesByLevel(String level) {
+        List<Course> allCourses = courseRepo.findAll();
+        if (allCourses == null) return List.of();
+
+        return allCourses.stream()
+                .filter(c -> c.getLevel().name().equalsIgnoreCase(level)) // Tiìm theo level
+                .collect(Collectors.toList());
+    }
+}
