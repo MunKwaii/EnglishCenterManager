cdae31864b5ff3474678aebfe8192bd19038f42d
Khanh <Quockhanh6310@gmail.com>
2026-03-04 15:47:48 +0700

    Update interface teacher service

--- src/main/java/vn/edu/ute/service/TeacherService.java
+++ src/main/java/vn/edu/ute/service/TeacherService.java
@@ -1,4 +1,14 @@
 package vn.edu.ute.service;
 
+import vn.edu.ute.model.Teacher;
+
+import java.util.List;
+
 public interface TeacherService {
+    List<Teacher> findBySpecialty(String specialty);
+    boolean isMailExist(String mail);
+    List<String> getAllTeacherNames();
+    Teacher findByPhone(String phone);
+    long countActiveTeachers();
+    List<Teacher> getSortedTeachers();
 }
