9df64145925ea0200fbf077b11746c5da82dcb44
Khanh <Quockhanh6310@gmail.com>
2026-03-04 15:21:18 +0700

    Code teacher repository

--- src/main/java/vn/edu/ute/repository/TeacherRepository.java
+++ src/main/java/vn/edu/ute/repository/TeacherRepository.java
@@ -1,5 +1,13 @@
 package vn.edu.ute.repository;
 
-public interface TeacherRepository {
+import vn.edu.ute.model.Teacher;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface TeacherRepository {
+    void save(Teacher teacher);
+    Optional<Teacher> findById(Long id);
+    List<Teacher> findAll();
+    void deleteById(Long id);
 }
