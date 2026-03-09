07999cb5c582610d8d6527d98e2bbcaac2cdd3b3
NGUYỄN QUỐC KHÁNH <Quockhanh6310@gmail.com>
2026-03-09 23:41:32 +0700

    Trim trailing whitespace in TestLoginUI.java
    Remove an extraneous trailing space from the System.setProperty line in TestLoginUI.java. This is a whitespace-only cleanup and does not change program behavior.

--- src/main/java/vn/edu/ute/view/TestLoginUI.java
+++ src/main/java/vn/edu/ute/view/TestLoginUI.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class TestLoginUI {
     public static void main(String[] args) {
         // dòng này để tự động scale giao diện trên màn hình độ phân giải cao
-        System.setProperty("sun.java2d.uiScale", "2.0"); 
+        System.setProperty("sun.java2d.uiScale", "2.0");
 
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
